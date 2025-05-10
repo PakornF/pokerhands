6002393d45a64e4dfa8505917830dbcc8a82ea37
Pakorn Fudulyawajananont <pakorn.f@ku.th>
2025-05-10 20:41:28 +0700

    rm player

--- player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self):
-        self.cards = []
-        self.name = ""
-        self.chips = 1000  # Starting chips
-        self.current_bet = 0
-        self.small_blind = False
-        self.big_blind = False
-        self.folded = False
